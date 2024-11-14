ada58f19bf04e681dcf3837443835b1a83a45591
Camila Nunes <cvn.camila@gmail.com>
2024-11-14 17:08:14 -0300

    ajustes;

---
+++ src/app/cadastros/cartao-de-credito/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartaoDeCreditoPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Cartão de Crédito</h1>
+      {/* Add your component content here */}
+    </div>
+  );
+};
+
+export default CartaoDeCreditoPage;
---
+++ src/app/cadastros/contas-banco/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ContasBancoPage() {
+  return (
+    <div>
+      <h1>Contas Banco</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+}
